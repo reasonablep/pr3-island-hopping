fa040c21c62baab1ae785b6f2afe0e2db27d574e
reasonablep <140934359+reasonablep@users.noreply.github.com>
2024-01-25 19:12:13 -0800

    changed seeds.js to reflect new model defs

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,150 +1,150 @@
 const db = require('./connection');
-const { User, Product, Category } = require('../models');
+const { User, Island, Category } = require('../models');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   await cleanDB('Category', 'categories');
-  await cleanDB('Product', 'products');
+  await cleanDB('Island', 'islands');
   await cleanDB('User', 'users');
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: 'Tropical' },
+    { name: 'Hunting' },
+    { name: 'Cold-Weather' },
+    { name: 'Isolated' },
+    { name: 'Oversized' }
   ]);
 
   console.log('categories seeded');
 
-  const products = await Product.insertMany([
+  const islands = await Island.insertMany([
     {
-      name: 'Tin of Cookies',
-      description:
+      islandName: 'Tin of Cookies',
+      islandDescription:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+      location: 'cookie-tin.jpg',
       category: categories[0]._id,
       price: 2.99,
-      quantity: 500
+      quantity: 1
     },
     {
-      name: 'Canned Coffee',
-      description:
+      islandName: 'Canned Coffee',
+      islandDescription:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+      location: 'canned-coffee.jpg',
       category: categories[0]._id,
       price: 1.99,
-      quantity: 500
+      quantity: 1
     },
     {
-      name: 'Toilet Paper',
+     islandName: 'Toilet Paper',
       category: categories[1]._id,
-      description:
+      islandDescription:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
+      location: 'toilet-paper.jpg',
       price: 7.99,
-      quantity: 20
+      quantity: 1
     },
     {
-      name: 'Handmade Soap',
+      islandName: 'Handmade Soap',
       category: categories[1]._id,
-      description:
+      islandDescription:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
+      location: 'soap.jpg',
       price: 3.99,
-      quantity: 50
+      quantity: 1
     },
     {
-      name: 'Set of Wooden Spoons',
+      islandName: 'Set of Wooden Spoons',
       category: categories[1]._id,
-      description:
+      islandDescription:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
+      location: 'wooden-spoons.jpg',
       price: 14.99,
-      quantity: 100
+      quantity: 1
     },
     {
-      name: 'Camera',
+      islandName: 'Camera',
       category: categories[2]._id,
-      description:
+      islandDescription:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
+      location: 'camera.jpg',
       price: 399.99,
       quantity: 30
     },
     {
-      name: 'Tablet',
+      islandName: 'Tablet',
       category: categories[2]._id,
-      description:
+      islandDescription:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
+      location: 'tablet.jpg',
       price: 199.99,
       quantity: 30
     },
     {
-      name: 'Tales at Bedtime',
+      islandName: 'Tales at Bedtime',
       category: categories[3]._id,
-      description:
+      islandDescription:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
+      location: 'bedtime-book.jpg',
       price: 9.99,
-      quantity: 100
+      quantity: 1
     },
     {
-      name: 'Spinning Top',
+      islandName: 'Spinning Top',
       category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
+      islandDescription: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
+      location: 'spinning-top.jpg',
       price: 1.99,
-      quantity: 1000
+      quantity: 1
     },
     {
-      name: 'Set of Plastic Horses',
+      islandName: 'Set of Plastic Horses',
       category: categories[4]._id,
-      description:
+      islandDescription:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
+      location: 'plastic-horses.jpg',
       price: 2.99,
-      quantity: 1000
+      quantity: 1
     },
     {
-      name: 'Teddy Bear',
+      islandName: 'Teddy Bear',
       category: categories[4]._id,
-      description:
+      islandDescription:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
+      location: 'teddy-bear.jpg',
       price: 7.99,
-      quantity: 100
+      quantity: 1
     },
     {
-      name: 'Alphabet Blocks',
+      islandName: 'Alphabet Blocks',
       category: categories[4]._id,
-      description:
+      islandDescription:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
+      location: 'alphabet-blocks.jpg',
       price: 9.99,
-      quantity: 600
+      quantity: 1
     }
   ]);
 
-  console.log('products seeded');
+  console.log('islands seeded');
 
   await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
+    firstName: 'Peter',
+    lastName: 'Martinez',
+    email: 'pmrtnz@me.com',
     password: 'password12345',
     orders: [
       {
-        products: [products[0]._id, products[0]._id, products[1]._id]
+        islands: [islands[0]._id, islands[0]._id, islands[1]._id]
       }
     ]
   });
 
   await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
+    firstName: 'Jon',
+    lastName: 'Sargis',
+    email: 'jsargis@hotmail.com',
     password: 'password12345'
   });
 
