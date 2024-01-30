b343f4f9e3fb8e619d189e927a71eaccd14fcad5
reasonablep <140934359+reasonablep@users.noreply.github.com>
2024-01-29 20:46:00 -0800

    Got front-end working after lots of troubleshooting, failed to use branch, pushing up current revision with all group members present

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,47 @@
-import { Outlet } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import { ThemeProvider} from '@mui/system';
+import { Box, Container, Typography } from '@mui/material';
+import { useEffect } from 'react';
+import theme from '../src/styles/theme/index';
+import Appbar from '../src/components/appbar/index.jsx';
+import Banner from '../src/components/appbar/banner/index.jsx';
+import Promotions from '../src/components/promotions/index.jsx';
+import Islands from '../src/components/islands/index.jsx';
+import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Prime Isles Estate: Elevate Your Lifestyle with Exclusive Private Islands-Home"
+  }, 
+  []);
+
   return (
-    <ApolloProvider client={client}>
-      <div>
-        <StoreProvider>
-          <Nav />
-          <Outlet />
-        </StoreProvider>
-      </div>
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+    <Container  
+      maxWidth="x1"
+      sx={{
+        background: '#fff'
+      }}
+    >
+      <Appbar />
+      <Banner />
+      <Promotions />
+      <Box display="flex" justifyContent={"center"} sx={{ p: 4 }}>
+        <Typography variant='h4'>Our Islands</Typography>
+      </Box>
+      <Islands />
+     
+      {
+        /*
+        Title
+        Products
+        Footer
+        Search box
+        App drawer
+        */
+      }
+    </Container>
+    </ThemeProvider>
   );
 }
 
